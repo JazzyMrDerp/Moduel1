21306cb183030393f40f4dceb1e167b421937aa3
JazzyMrDerp <jamehta19@gmail.com>
2024-02-11 16:40:59 -0500

    Project
    Copy pasted all the things

--- Modeul1_0/src/Main.java
+++ Modeul1_0/src/Main.java
@@ -2,14 +2,67 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        Declaring variables
+        int age = 25;
+        double salary = 50000.0;
+        String name = "John";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//        For loops
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+//        While loop:
+        int count = 0;
+        while (count < 3) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+
+//        Do-While loop:
+        int num = 1;
+        do {
+            System.out.println("Number: " + num);
+            num++;
+        } while (num <= 3);
+
+//        If statement:
+        int x = 10;
+        if (x > 5) {
+            System.out.println("x is greater than 5");
+        }
+
+//        If-Else Statement:
+        int num_ = 7;
+        if (num_ % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+//        Nested If-Else:
+        int age_ = 18;
+        if (age_ >= 18) {
+            if (age_ < 21) {
+                System.out.println("You're eligible but not for alcohol");
+            } else {
+                System.out.println("You're eligible for everything");
+            }
+        } else {
+            System.out.println("You're not eligible");
+        }
+
+//        Switch Case:
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+// ...
+            default:
+                System.out.println("Invalid day");
         }
     }
 }
